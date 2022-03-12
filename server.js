a08294067198fd05c7db81569e49dd900b7dc2de
Toan Pham Phu <phamphutoan98@gmail.com>
2022-03-12 13:53:24 +0700

    Solution

--- server.js
+++ server.js
@@ -25,7 +25,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+// solution
+app.get('/api/whoami', (req, res) => {
+  console.log(req.headers);
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
